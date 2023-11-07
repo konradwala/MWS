d4be51c934c015dfa4c50870ae91ed50707dfaab
konradwala <konradwala1@gmail.com>
2023-11-07 17:36:51 +0100

    Initial commit

---
+++ MWS_app/MWS_app/MainWindow.xaml.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Windows;
+using System.IO.Ports;
+using System.Collections.ObjectModel;
+using System.Windows.Input;
+using System.Windows.Controls;
+using System.Collections.Specialized;
+
+namespace StepperControlApp
+{
+    public partial class MainWindow : Window
+    {
+        SerialPort serialPort;
+        ObservableCollection<string> availablePorts = new ObservableCollection<string>();
+
+        private bool isArrowButtonPressed = false;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            string[] ports = SerialPort.GetPortNames();
+            foreach (string port in ports)
+            {
+                availablePorts.Add(port);
+            }
+
+            PortComboBox.ItemsSource = availablePorts;
+        }
+
+        private void OpenPortButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (PortComboBox.SelectedItem != null)
+            {
+                string selectedPort = PortComboBox.SelectedItem.ToString();
+                serialPort = new SerialPort(selectedPort, 9600);
+
+                try
+                {
+                    serialPort.Open();
+                    StatusTextBlock.Text = $"Port {selectedPort} otwarty.";
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show($"Nie można otworzyć portu szeregowego: {ex.Message}", "Błąd", MessageBoxButton.OK, MessageBoxImage.Error);
+                }
+            }
+            else
+            {
+                MessageBox.Show("Wybierz port przed otwarciem.", "Błąd", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+
+        private void ArrowButton_Pressed(object sender, RoutedEventArgs e)
+        {
+            if (serialPort != null && serialPort.IsOpen)
+            {
+                isArrowButtonPressed = true;
+                string command = ((Button)sender).Tag.ToString();
+                serialPort.WriteLine(command);
+            }
+        }
+
+        private void ArrowButton_Released(object sender, RoutedEventArgs e)
+        {
+            if (serialPort != null && serialPort.IsOpen)
+            {
+                isArrowButtonPressed = false;
+                serialPort.WriteLine("STOP");
+            }
+        }
+
+        protected override void OnClosing(System.ComponentModel.CancelEventArgs e)
+        {
+            if (serialPort != null && serialPort.IsOpen)
+            {
+                serialPort.Close();
+            }
+            base.OnClosing(e);
+        }
+
+
+    }
+}
