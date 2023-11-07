fab6cdcb7e2a74bedf32a9268176f55ff9cc12c2
konradwala <konradwala1@gmail.com>
2023-11-07 17:44:27 +0100

    Correct version with one horizontal engine

--- MWS_arduino/MWS_arduino.ino
+++ MWS_arduino/MWS_arduino.ino
@@ -1,25 +1,16 @@
 #include <Stepper.h>
-#include <pcf8574.h>
 
-PCF8574 ex1(0x20);
-
-// Define the pins for the second stepper motor
+// Define the pins for the first stepper motor
 #define motor2Pin1 6
 #define motor2Pin2 7
 #define motor2Pin3 8
 #define motor2Pin4 9
 
-// Define the pins for the first stepper motor
+// Define the pins for the second stepper motor
 #define motor1Pin1 10
 #define motor1Pin2 11
 #define motor1Pin3 12
 #define motor1Pin4 13
-
-// Define the pins for the third stepper motor
-#define motor3Pin1 2
-#define motor3Pin2 3
-#define motor3Pin3 4
-#define motor3Pin4 5
 
 // Define the number of steps per revolution for the stepper motors
 const int stepsPerRevolution = 32;
@@ -28,7 +19,6 @@ int stepMinus = -1;
 
 Stepper stepper1(stepsPerRevolution, motor1Pin1, motor1Pin3, motor1Pin2, motor1Pin4);
 Stepper stepper2(stepsPerRevolution, motor2Pin1, motor2Pin3, motor2Pin2, motor2Pin4);
-Stepper stepper2(stepsPerRevolution, motor3Pin1, motor3Pin3, motor3Pin2, motor3Pin4);
 
 void setup() {
   // Initialize the serial communication
@@ -37,7 +27,6 @@ void setup() {
   // Set the motor speed (RPM)
   stepper1.setSpeed(800);
   stepper2.setSpeed(800);
-  stepper3.setSpeed(800);
 }
 
 void loop() {
@@ -48,12 +37,10 @@ void loop() {
     if (command == "STEPPER1_LEFT") {
       while (Serial.available() == 0) {
         stepper1.step(stepPlus); // Move the first motor left by 1 step
-        stepper3.step(stepMinus); 
       }
     } else if (command == "STEPPER1_RIGHT") {
       while (Serial.available() == 0) {
         stepper1.step(stepMinus); // Move the first motor right by 1 step
-        stepper2.step(stepPlus);
       }
     } else if (command == "STEPPER2_UP") {
       while (Serial.available() == 0) {
